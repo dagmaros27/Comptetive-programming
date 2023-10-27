a94aa8e723e17bebe27325ec2dc28d3b1746946a
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2023-10-27 15:05:19 +0300

    group-anagrams

---
+++ group-anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]
+:
+        dic = collections.defaultdict(list)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            dic[sorted_word].append(word)
+        
+        return list(dic.values())
+            
