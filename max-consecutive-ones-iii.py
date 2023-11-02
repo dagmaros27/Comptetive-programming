c7c75474e38017330a6e3aead8ae5483e102d2fb
Dagmawi Misker <99316751+dagmaros27@users.noreply.github.com>
2023-11-02 09:17:09 +0300

    max-consecutive-ones-iii

---
+++ max-consecutive-ones-iii.py
@@ -0,0 +1,45 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+            #brute force
+            #hard to find the brute force
+
+
+            hmap = defaultdict(int)
+            length = 0
+            l =0
+            for r in range(len(nums)):
+                hmap[nums[r]] += 1
+
+                while hmap[0] > k:
+                    hmap[nums[l]] -= 1
+                    l += 1
+                length = max(length, hmap[0] + hmap[1])
+
+            return length 
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+            #brute force
+            # start = 0
+
+
+            hmap = defaultdict(int)
+            length = 0
+            l =0
+            for r in range(len(nums)):
+                hmap[nums[r]] += 1
+            # for end in range(len(nums)):
+            #     if nums[end] == 0:
+            #         k -= 1
+            #     if k < 0:
+            #         if nums[start] == 0:
+            #             k += 1
+            #         start += 1
+            # return end - start + 1
+
+                while hmap[0] > k:
+                    hmap[nums[l]] -= 1
+                    l += 1
+                length = max(length, hmap[0] + hmap[1])
+
+            return length 
